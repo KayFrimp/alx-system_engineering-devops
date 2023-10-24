12a71a0509dbd1cea341111b6eb163253b8fce0b
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-23 17:42:24 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    # Fetch User data
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_res = requests.get(f'{url}users/{user_id}')
+    user_info = user_res.json()
+    name = user_info.get('name')
+
+    # Fetch todo data
+    todo_res = requests.get(f'{url}todos?userId={user_id}')
+    todo_info = todo_res.json()
+
+    total_tasks = len(todo_info)
+    done_tasks = len([task
+                      for task in todo_info if task.get('completed') is True])
+
+    if name:
+        print(
+            f'Employee {name} is done with tasks({done_tasks}/{total_tasks}):')
+        for task in todo_info:
+            if task.get('completed'):
+                print('\t ' + task.get('title'))
