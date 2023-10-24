30c4e75f3eb67853864444e8d656d2a7eaaf8283
Kwabena A. Frimpong <kafrimpong23@gmail.com>
2023-10-23 18:24:13 -0700

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Script exports information on Employee to CSV"""
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+
+    # Fetch User data
+    url = 'https://jsonplaceholder.typicode.com/'
+    user_res = requests.get(f'{url}users/{user_id}')
+    user_info = user_res.json()
+    name = user_info.get('name')
+
+    # Fetch todo data
+    todo_res = requests.get(f'{url}todos?userId={user_id}')
+    todo_info = todo_res.json()
+
+    # Open a CSV file with the user ID as it's name
+    with open(f'{user_id}.csv', 'w', newline='') as file:
+        writer = csv.writer(file,
+                            delimiter=',',
+                            quotechar='"', quoting=csv.QUOTE_ALL)
+
+        for task in todo_info:
+            task = [user_id, name, task.get('completed'), task.get('title')]
+            writer.writerow(task)
